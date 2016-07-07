bb11a292d62e23ff69b960fbc55d033ce463a8aa
Sumo <somedutta.ghosh@innoraft.com>
2016-07-08 01:50:23 +0530

    curtain fixes

--- js/general.js
+++ js/general.js
@@ -7,10 +7,10 @@
 		$winW = function(){ return $(window).width() },
 		$winH = function(){ return $(window).height() },
 		$mainmenu = $('#mainmenu'),
-		$screensize = function(element){  
+		$screensize = function(element){
 			$(element).width($winW()).height($winH());
 		};
-		
+
 		var screencheck = function(mediasize){
 			if (typeof window.matchMedia !== "undefined"){
 				var screensize = window.matchMedia("(max-width:"+ mediasize+"px)");
@@ -29,21 +29,21 @@
 		};
 
 	$doc.ready(function() {
-/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
 		// Remove No-js Class
 		$("html").removeClass('no-js').addClass('js');
-		
-		
-		
+
+
+
 		/* Get Screen size
 		---------------------------------------------------------------------*/
 		$win.load(function(){
 			$win.on('resize', function(){
-				$screensize('your selector');	
-			}).resize();	
+				$screensize('your selector');
+			}).resize();
 		});
-		
-		
+
+
 		/* Menu ICon Append prepend for responsive
 		---------------------------------------------------------------------*/
 		$(window).on('resize', function(){
@@ -51,13 +51,13 @@
 				if(!$('#menu').length){
 					$('#mainmenu .wrap').prepend('<a href="#" id="menu" class="menulines-button"><span class="menulines"></span> <em>Menu</em></a>');
 				}
-				
+
 			} else {
 				$("#menu").remove();
 			}
 		}).resize();
 
-		/* This adds placeholder support to browsers that wouldn't otherwise support it. 
+		/* This adds placeholder support to browsers that wouldn't otherwise support it.
 		---------------------------------------------------------------------*/
 		if (document.createElement("input").placeholder == undefined) {
 			var active = document.activeElement;
@@ -76,9 +76,9 @@
 				$(':text.hasPlaceholder').val('');
 			});
 		}
-		
-		
-		/* Tab Content box 
+
+
+		/* Tab Content box
 		---------------------------------------------------------------------*/
 		var tabBlockElement = $('.tab-data');
 			$(tabBlockElement).each(function(index, element) {
@@ -88,26 +88,26 @@
 					var textval = new Array();
 					tabTrigger.each(function() {
 						textval.push( $(this).text() );
-					});	
+					});
 				$this.find(tabTrigger).first().addClass("active");
 				$this.find(tabContent).first().show();
 
-				
+
 				$(tabTrigger).on('click',function () {
 					$(tabTrigger).removeClass("active");
 					$(this).addClass("active");
 					$(tabContent).hide().removeClass('visible');
 					var activeTab = $(this).find("a").attr("data-rel");
 					$this.find('#' + activeTab).fadeIn('normal').addClass('visible');
-								
+
 					return false;
 				});
-			
+
 				var responsivetabActive =  function(){
 				if (screencheck(767)){
 					if( !$('.tabMobiletrigger').length ){
 						$(tabContent).each(function(index, element) {
-							$(this).before("<h2 class='tabMobiletrigger'>"+textval[index]+"</h2>");	
+							$(this).before("<h2 class='tabMobiletrigger'>"+textval[index]+"</h2>");
 							$this.find('.tabMobiletrigger:first').addClass("rotate");
 						});
 						$('.tabMobiletrigger').click('click', function(){
@@ -125,12 +125,12 @@
 							return false;
 						})
 					}
-						
+
 				}
 				if ( $winW() > 768 ){
 					$('.tabMobiletrigger').remove();
 					$this.find(tabTrigger).removeClass("active").first().addClass('active');
-					$this.find(tabContent).hide().first().show();		
+					$this.find(tabContent).hide().first().show();
 				}
 			}
 			$(window).on('resize', function(){
@@ -139,17 +139,17 @@
 				}
 			}).resize();
 		});
-		
+
 		/* Accordion box JS
 		---------------------------------------------------------------------*/
 		$('.accordion-databox').each(function(index, element) {
 			var $accordion = $(this),
 			$accordionTrigger = $accordion.find('.accordion-trigger'),
 			$accordionDatabox = $accordion.find('.accordion-data');
-			
+
 			$accordionTrigger.first().addClass('open');
 			$accordionDatabox.first().show();
-			
+
 			$accordionTrigger.on('click',function (e) {
 				var $this = $(this);
 				var $accordionData = $this.next('.accordion-data');
@@ -165,42 +165,142 @@
 				}
 			})
 		});
-		
+
 		/* FullPage Js
 		---------------------------------------------------------------------*/
-     $('#fullpage').fullpage({
-            // Events.
-			onLeave: function(index, nextIndex, direction){
-				if(index==2 && direction=='up'){
-					$('.curtain').hide();	
-				} else {
-					$('.curtain').show();	
-					$('.curtain').addClass('full').removeClass('fullWidth');
-					$('.text-placeholder').html('');  
-				}
-			},
-            afterLoad: function(anchorLink, index){
-					setTimeout(function() {
-					$('.curtain').removeClass('fullWidth full');
-					$('.curtain').addClass('xswidth');
-					
-					if (index%2 == 0) { 
-						$('.curtain').addClass('left-align');
-					} else { 
-						$('.curtain').removeClass('left-align');
-					}
-					
-					var $bg = $('.section').eq(index-1).attr('data-rel');
-					$('.curtain').css('background-color',$bg);
-					$('.section').eq(index-1).parents('#fullpage').css('background-color',$bg);
-					
-					text = $('.section').eq(index-1).find('.text-container').html();
-					$('.text-placeholder').html(text);
-					$('.text-placeholder').fadeIn('slow');
-				  },300);
-				  setTimeout(function(){ 
-					$('.text-placeholder').each(function() {
-						var $this = $(this);
+   //   $('#fullpage').fullpage({
+   //          // Events.
+			// onLeave: function(index, nextIndex, direction){
+			// 	$('.curtain').addClass('full');
+   //  //     $('.text-placeholder').fadeOut('slow');
+
+			// 	// if(index==2 && direction=='up'){
+			// 	// 	$('.curtain').hide();
+			// 	// } else {
+			// 	// 	$('.curtain').show();
+			// 	// 	$('.curtain').addClass('full').removeClass('fullWidth');
+			// 	// 	$('.text-placeholder').html('');
+			// 	// }
+			// },
+   //    afterLoad: function(anchorLink, index){
+   //    	setTimeout(function() {
+   //        $('.curtain').removeClass('full');
+   //        $('.curtain').toggleClass('white');
+   //        setTimeout(function() {
+	  //         $('.curtain').toggleClass('black');
+	  //       },300);
+
+
+			// 		// text = $('.section').eq(index-1).find('.text-container').html();
+			// 		// $('.text-placeholder').html(text);
+   //   //      $('.text-placeholder').fadeIn('slow');
+   //      },300);
+
+			// 		// setTimeout(function() {
+			// 		// $('.curtain').removeClass('fullWidth full');
+			// 		// $('.curtain').addClass('xswidth');
+
+			// 		// if (index%2 == 0) {
+			// 		// 	$('.curtain').addClass('left-align');
+			// 		// } else {
+			// 		// 	$('.curtain').removeClass('left-align');
+			// 		// }
+
+			// 		// var $bg = $('.section').eq(index-1).attr('data-rel');
+			// 		// $('.curtain').css('background-color',$bg);
+			// 		// $('.section').eq(index-1).parents('#fullpage').css('background-color',$bg);
+
+			// 		// text = $('.section').eq(index-1).find('.text-container').html();
+			// 		// $('.text-placeholder').html(text);
+			// 		// $('.text-placeholder').fadeIn('slow');
+			// 	 //  },300);
+
+
+			// 	 //  setTimeout(function(){
+			// 		// 	$('.text-placeholder').each(function() {
+			// 		// 		var $this = $(this);
+			// 		// 			$this.find('.animate').each(function(i) {
+			// 		// 				var $item = $(this);
+			// 		// 				var animation = $item.data("animate");
+			// 		// 				setTimeout( function () {
+			// 		// 					$item.addClass('animated '+animation).removeClass('animate');
+			// 		// 				}, i*100 );
+			// 		// 			});
+			// 		// 		});
+			// 		// }, 500);
+
+   //        },
+   //        afterRender: function(){
+   //        	setTimeout(function() {
+   //            // $('.curtain').addClass('white');
+   //            // $('.curtain').removeClass('black');
+
+   //     //        text = $('.section').eq(index-1).find('.text-container').html();
+			// 				// $('.text-placeholder').html(text);
+   //     //        $('.text-placeholder').fadeIn('slow');
+   //          },300);
+
+   //    //     	setTimeout(function(){
+			// 				// $('.section').eq(0).each(function() {
+			// 				// 	var $this = $(this);
+			// 				// 	$this.find('.animate').each(function(i) {
+			// 				// 			var $item = $(this);
+			// 				// 			var animation = $item.data("animate");
+			// 				// 			setTimeout( function () {
+			// 				// 				$item.addClass('animated '+animation).removeClass('animate');
+			// 				// }, i*100 );
+			// 			// });
+			// // 		});
+			// // }, 400);
+			// 		},
+   //      });
+   //
+      $('#fullpage').fullpage({
+        // Events.
+        onLeave: function(index, nextIndex, direction){
+
+          if (curtain_visibility(index, direction)) {
+            $('.curtain').removeClass('open');
+            $('.curtain').addClass('full');
+          }
+          else {
+            $('.curtain').addClass('full');
+            $('.curtain').addClass('open');
+          }
+          $('.text-placeholder').fadeOut('slow');
+        },
+        afterLoad: function(anchorLink, index){
+          setTimeout(function() {
+            $('.curtain').removeClass('full');
+            $('.curtain').toggleClass('white');
+            $('.curtain').toggleClass('black');
+            text = $('.section').eq(index-1).find('.text-container').html();
+            $('.text-placeholder').html(text);
+            $('.text-placeholder').fadeIn('slow');
+          },300);
+          setTimeout(function(){
+            $('.text-placeholder').each(function() {
+	            var $this = $(this);
+	            $this.find('.animate').each(function(i) {
+	              var $item = $(this);
+	              var animation = $item.data("animate");
+	              console.log(animation);
+	              setTimeout( function () {
+	                $item.addClass('animated '+animation).removeClass('animate');
+	              }, i*100 );
+	            });
+            });
+          }, 500);
+        },
+        afterRender: function(index){
+          setTimeout(function() {
+            $('.curtain').addClass('open');
+            $('.curtain').addClass('white');
+            $('.curtain').removeClass('black');
+          },300);
+          setTimeout(function(){
+						$('.section').eq(0).each(function() {
+							var $this = $(this);
 							$this.find('.animate').each(function(i) {
 								var $item = $(this);
 								var animation = $item.data("animate");
@@ -209,28 +309,32 @@
 								}, i*100 );
 							});
 						});
-				}, 500);
+					}, 400);
+        },
+        afterResize: function(){},
+      });
 
-            },
-            afterRender: function(){
-            	setTimeout(function(){ 
-					$('.section').eq(0).each(function() {
-						var $this = $(this);
-							$this.find('.animate').each(function(i) {
-								var $item = $(this);
-								var animation = $item.data("animate");
-								setTimeout( function () {
-									$item.addClass('animated '+animation).removeClass('animate');
-								}, i*100 );
-							});
-						});
-				}, 400);
-			},
-            afterResize: function(){},
-        });
+      function curtain_visibility(index, direction) {
+        if (direction == 'down') {
+          if ($('.section').eq(index).hasClass('hash-point')) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+        else {
+          if ($('.section').eq(index-2).hasClass('hash-point')) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+      }
 
 
-/*--------------------------------------------------------------------------------------------------------------------------------------*/		
-	});	
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+	});
 /*--------------------------------------------------------------------------------------------------------------------------------------*/
 })(jQuery, window, document);
